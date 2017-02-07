d0f4e2dc6324895fd6612af3f1376b4e2c6dd731
Sophia <swoozie14.com>
2017-02-07 12:23:16 -0800

    Add Events model/controller and basic event functionality

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,29 @@
+class EventsController < ApplicationController
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+    if @event.save
+      flash[:success] = 'Event successfully created'
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+    def event_params
+      params.require(:event).permit(:title, :description)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170206212549) do
+ActiveRecord::Schema.define(version: 20170207195540) do
+
+  create_table "events", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "date"
+    t.string   "location"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "creator_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
