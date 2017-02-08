814627fc05e516d773e548fe3fa318bc52fde156
Sophia <swoozie14.com>
2017-02-08 14:38:04 -0800

    Add more styling to views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  get '/newevent', to: 'events#new'
+  post '/newevent', to: 'events#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
