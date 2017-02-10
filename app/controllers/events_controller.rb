672925fb32bc4504075db6304cacc58e8cd0348b
Sophia <swoozie14.com>
2017-02-10 09:28:59 -0800

    Default event creator as attendee

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,6 +9,7 @@ class EventsController < ApplicationController
     @event = current_user.created_events.build(event_params)
     if @event.save
       flash[:success] = 'Event successfully created'
+      @event.attendees << current_user
       redirect_to current_user
     else
       render 'new'
